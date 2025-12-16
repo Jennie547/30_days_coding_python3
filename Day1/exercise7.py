a4d14d6926971843abc2c22bde1ed01bc5cfc996
Jennie547 <jessica4edigin@gmail.com>
2025-12-16 05:38:37 +0100

    day 1 accomplished

---
+++ Day1/exercise7.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Write a program that asks the user for a weight in kilograms and converts it to pounds. There are 2.2 pounds in a kilogram.
+
+weight = eval(input("Enter your weight in kilograms: "))
+pounds = weight * 2.2
+
+print("You weigh, in pounds:", pounds, sep=" ")
+
